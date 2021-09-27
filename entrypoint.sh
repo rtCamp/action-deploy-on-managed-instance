4be96c54cf8a7ff44aebd6ba6edf4d390b1c0cfd
dhsathiya <devarshisathiya5@gmail.com>
2021-09-27 20:50:41 +0530

    Fix rsync
    Signed-off-by: dhsathiya <devarshisathiya5@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,13 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 
 if [ -z "$NPM_VERSION" ]
 then
-    
+
     NPM_VERSION="latest"
     export npm_install=$NPM_VERSION
     curl -L https://www.npmjs.com/install.sh | sh > /dev/null 2>&1
 
 else
-    
+
     export npm_install=$NPM_VERSION
     curl -L https://www.npmjs.com/install.sh | sh > /dev/null 2>&1
 
@@ -28,12 +28,12 @@ fi
 # Installing Specified version of Node.js
 if [ -z "$NODE_VERSION" ]
 then
-    
+
     NODE_VERSION=latest
     nvm install node
 
 else
-    
+
     # Installing Specified version of Node.js
     echo "Installing Specified version of Node.js"
     nvm install $NODE_VERSION
@@ -51,29 +51,29 @@ fi
 
 if [ -z "$BUILD_DIRECTORY" ]
 then
-      
+
     echo "BUILD_DIRECTORY environment variable not set"
 
 else
-    
+
     if [ -z "$BUILD_COMMAND" ]
     then
-    
+
         echo "BUILD_COMMAND environment variable not set"
-    
+
     else
-    
+
         cd $BUILD_DIRECTORY
         ls -la
         $BUILD_COMMAND
-    
+
     fi
 
 fi
 
 if [ -z "$BUILD_SCRIPT" ]
 then
-    
+
     echo "BUILD_SCRIPT environment variable not set"
 
 else
@@ -93,14 +93,12 @@ export GITHUB_BRANCH=${GITHUB_REF##*heads/}
 hosts_file="$GITHUB_WORKSPACE/.github/hosts.yml"
 export hostname=$(cat "$hosts_file" | shyaml get-value "$GITHUB_BRANCH.hostname")
 export ssh_user=$(cat "$hosts_file" | shyaml get-value "$GITHUB_BRANCH.user")
-
-# Extracted from YML file
-# echo $hostname
-# echo $ssh_user
+export single_deploy_location=$(cat "$hosts_file" | shyaml get-value "$GITHUB_BRANCH.single_deploy_location")
 
 #================================================================#
 #                    Setting up SSH                              #
 #================================================================#
+
 
 mkdir $HOME/.ssh
 chmod 600 ~/.ssh
@@ -119,18 +117,64 @@ EOL
 
 ssh-keyscan "$hostname" >> ${SSH_DIR}/known_hosts
 
-DEPLOY_LOCATIONS=$GITHUB_WORKSPACE/.github/locations.csv
+#================================================================#
+#                    Deployment                                  #
+#================================================================#
 
-# Splitting Space separated String
-echo "$DEPLOY_LOCATIONS this is deploy location"
 
-while read line;
-do
+if [ -z $single_deploy_location ]
+then
+    DEPLOY_LOCATIONS=$GITHUB_WORKSPACE/.github/locations.csv
 
-    source=$(echo $line | awk -F'[,]' '{print $1}')
-    destination=$(echo $line | awk -F'[,]' '{print $2}')
-    echo $source
-    echo $destination
-    rsync -avzhp -e "ssh -i $HOME/.ssh/id_rsa" $source $ssh_user@$hostname:$destination
+    # Splitting Space separated String
+    echo "$DEPLOY_LOCATIONS this is deploy location"
 
-done <<< "$(cat $DEPLOY_LOCATIONS)"
+    while read line;
+    do
+
+        source=$(echo $line | awk -F'[,]' '{print $1}')
+        destination=$(echo $line | awk -F'[,]' '{print $2}')
+        echo $source
+        echo $destination
+        rsync -avzhP -e "ssh -i $HOME/.ssh/id_rsa" \
+            --exclude '.git' \
+            --exclude '.github' \
+            --exclude 'deploy.php' \
+            --exclude 'composer.lock' \
+            --exclude '.env' \
+            --exclude '.env.example' \
+            --exclude '.gitignore' \
+            --exclude '.gitlab-ci.yml' \
+            --exclude 'Gruntfile.js' \
+            --exclude 'package.json' \
+            --exclude 'README.md' \
+            --exclude 'gulpfile.js' \
+            --exclude '.circleci' \
+            --exclude 'package-lock.json' \
+            --exclude 'package.json' \
+            --exclude 'phpcs.xml' \
+            --delete \
+            $source $ssh_user@$hostname:$destination/
+
+    done <<< "$(cat $DEPLOY_LOCATIONS)"
+else
+    rsync -avzhP -e "ssh -i $HOME/.ssh/id_rsa" \
+        --exclude '.git' \
+        --exclude '.github' \
+        --exclude 'deploy.php' \
+        --exclude 'composer.lock' \
+        --exclude '.env' \
+        --exclude '.env.example' \
+        --exclude '.gitignore' \
+        --exclude '.gitlab-ci.yml' \
+        --exclude 'Gruntfile.js' \
+        --exclude 'package.json' \
+        --exclude 'README.md' \
+        --exclude 'gulpfile.js' \
+        --exclude '.circleci' \
+        --exclude 'package-lock.json' \
+        --exclude 'package.json' \
+        --exclude 'phpcs.xml' \
+        --delete \
+        $source $ssh_user@$hostname:$single_deploy_location/
+fi
